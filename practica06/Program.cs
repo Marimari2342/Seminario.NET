611146696f235e700991be9820191faf79f8443c
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-26 10:37:26 -0300

    commit punto01

--- practica06/Program.cs
+++ practica06/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+internal class Program
+{
+    //PUNTO1
+    class A
+    {
+        protected int _id; //variable _id
+        public A(int id) => _id = id; //constructor que recibe valor id y lo guarda en _id
+        public virtual void Imprimir() => Console.WriteLine($"A_{_id}"); //podemos sobreescribirlo por --> [virtual]
+    }
+    class B : A
+    {
+        public B(int id) : base(id) { }
+        public override void Imprimir()
+        {
+            Console.Write($"B_{_id} --> ");
+            base.Imprimir();
+        }
+    }
+    class C : B
+    {
+        public C(int id) : base(id) { }
+        public override void Imprimir()
+        {
+            Console.Write($"C_{_id} --> ");
+            base.Imprimir();
+        }
+    }
+    class D : C
+    {
+        public D(int id) : base(id) { }
+        public override void Imprimir()
+        {
+            Console.Write($"D_{_id} --> ");
+            base.Imprimir();
+        }
+    }
+    private static void Main(string[] args)
+    {
+        A[] vector = [new A(3), new B(5), new C(15), new D(41)];
+        foreach (A a in vector)
+        {
+            a.Imprimir();
+        }
+    }
+}
+
--- practica06/readme.md
+++ practica06/readme.md
@@ -8,24 +8,34 @@
 ~~~c#
 class A
 {
-    protected int _id;
-    public A(int id) => _id = id;
-    public virtual void Imprimir() => Console.WriteLine($"A_{_id}");
+    protected int _id; //variable _id
+    public A(int id) => _id = id; //constructor que recibe valor id y lo guarda en _id
+    public virtual void Imprimir() => Console.WriteLine($"A_{_id}"); //podemos sobreescribirlo por --> [virtual]
 }
 class B : A
 {
-    . . .
+    public B(int id) : base(id){}
+    public override void Imprimir()
+    {
+        Console.Write($"B_{_id} --> ");
+        base.Imprimir();
+    }
 }
 class C : B
 {
-    . . .
+    public C(int id) : base(id){}
+    public override void Imprimir()
+    {
+        Console.Write($"C_{_id} --> ");
+        base.Imprimir();
+    }
 }
 class D : C
 {
-    . . .
+    public D(int id) : base(id){}
     public override void Imprimir()
     {
-        . . .
+        Console.Write($"D_{_id} --> ");
         base.Imprimir();
     }
 }
