a63c8d3a1ed8d583a960f6d75e9977ccf0905d6c
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-27 18:56:03 -0300

    commit procesador

--- practica07/Program.cs
+++ practica07/Program.cs
@@ -105,7 +105,7 @@ static class Procesador
     public static void Vender(IVendible x, Persona p) => x.SeVendeA(p);
     public static void Lavar(ILavable x) => x.SeLava();
     public static void Secar(ILavable x) => x.SeSeca();
-
-
+    public static void Reciclar(IReciclable x) => x.SeRecicla();
+    public static void Atender(IAtendible x) => x.SeAtiende();
 }
 
