cce2997adf2e3ca679c8f2f03cce6f57c5809970
Marimari2342 <marianroj@hotmail.com.ar>
2024-04-27 18:29:06 -0300

    commit libros

--- practica07/Program.cs
+++ practica07/Program.cs
@@ -28,6 +28,20 @@ interface IAtendibles
 }
 
 //Clases
-
+class Libro : IAlquilables, IReciclables
+{
+    public void esAlquilada()
+    {
+        Console.WriteLine("Alquilando libro a persona");
+    }
+    public void esDevuelta()
+    {
+        Console.WriteLine("Libro devuelto por persona");
+    }
+    public void esReciclada()
+    {
+        Console.WriteLine("Reciclando libro");
+    }
+}
 //Main
 
